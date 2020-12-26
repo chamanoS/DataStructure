0fc5452f80a3c86d343ff097e563a20019b84bec
chamano sidney <chamano.sr@gmail.com>
2020-12-26 20:40:41 +0200

    stack

--- script.js
+++ script.js
@@ -1,19 +1,61 @@
 //palindrome
 
-var arrLetters = [] ;
-var word = "madam" ;
-var rword = "" ;
+// var arrLetters = [] ;
+// var word = "madam" ;
+// var rword = "" ;
 
-for (var i = 0; i< word.length; i++){
-    arrLetters.push(word[i])
+// for (var i = 0; i< word.length; i++){
+//     arrLetters.push(word[i])
+// }
+
+// for (var i = 0; i< word.length; i++){
+//     rword += arrLetters.pop()
+// }
+
+// if(rword === word){
+//     console.log(word + " is a Palindrome")
+// } else {
+//     console.log(word + " is not a Palindrome")
+// }
+
+//creates a stack
+var Stack = function(){
+    this.count = 0; //keeps record of how many items are in the stack
+    this.storage = {};
+
+    //adds a value onto the end of the stack
+    this.push = function(value){
+        this.storage[this.count] = value;
+        this.count++;
+    }
+
+    //removes and returns the value at the end of the stack
+    this.pop = function(){
+        if(this.count === 0){
+            return undefined;
+        }
+
+        this.count--;
+        var result = this.storage[this.count];
+        delete this.storage[this.count];
+        return result;
+    }
+
+    this.size = function(){
+        return this.count;
+    }
+
+    //returns the value at the end of the stack
+    this.peek = function(){
+        return this.storage[this.count-1]
+    }
 }
 
-for (var i = 0; i< word.length; i++){
-    rword += arrLetters.pop()
-}
+var myStack = new Stack();
 
-if(rword === word){
-    console.log(word + " is a Palindrome")
-} else {
-    console.log(word + " is not a Palindrome")
-}
+myStack.push(20);
+myStack.push(2);
+console.log(myStack.peek())
+console.log(myStack.pop())
+console.log(myStack.peek())
+console.log(myStack.size())
